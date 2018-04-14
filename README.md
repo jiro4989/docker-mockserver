5b8544456b5f3405343306b45f146606b8641749
jiro4989 <jiroron666@gmail.com>
2018-04-14 11:54:37 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# docker-mockserver
+単一のJSONを返すモックサーバをdocker上で起動するテスト
+
+## 使い方
+下記のコマンドを実行してディレクトリ構造を確認する。
+
+```bash
+$ ls -lah
+合計 40K
+drwxr-xr-x  5 jiro jiro 4.0K  4月 14 11:46 .
+drwxr-xr-x 14 jiro jiro 4.0K  4月 14 08:01 ..
+-rw-r--r--  1 jiro jiro  589  4月 14 08:01 Gopkg.lock
+-rw-r--r--  1 jiro jiro  812  4月 14 08:01 Gopkg.toml
+-rw-r--r--  1 jiro jiro  125  4月 14 11:46 README.md
+drwxr-xr-x  2 jiro jiro 4.0K  4月 14 08:02 cmd
+-rw-r--r--  1 jiro jiro  217  4月 14 11:43 config.toml
+-rw-r--r--  1 jiro jiro  218  4月 14 08:06 docker-compose.yml
+drwxr-xr-x  4 jiro jiro 4.0K  4月 14 01:06 resp
+drwxr-xr-x  3 jiro jiro 4.0K  4月 14 08:01 vendor
+```
+
+下記のコマンドを実行してコンテナを起動する。
+
+```bash
+docker-compose up -d
+```
+
+下記のコマンドを実行してコンテナにログインする
+
+```bash
+docker exec -it mock bash
+```
+
+下記のコマンドを実行してコンテナ内でプログラムのあるディレクトリに移動する。
+
+```bash
+cd /go/src/mock
+```
+
+下記のコマンドを実行してサーバを起動する。
+
+```bash
+go run cmd/main.go
+```
+
+別で端末を起動して、下記のコマンドを実行する。
+
+```bash
+curl 172.17.0.1:8088/get/user.json
+```
+
+jsonが返ってきたら成功です。
+
---
+++ cmd/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/BurntSushi/toml"
+	"github.com/julienschmidt/httprouter"
+)
+
+type TOML struct {
+	Server Server `toml:"server"`
+	APIs   []API  `toml:"api"`
+}
+
+type Server struct {
+	Port string `toml:"port"`
+}
+
+type API struct {
+	Method string `toml:"method"`
+	URL    string `toml:"url"`
+}
+
+func main() {
+	var cfg TOML
+	if _, err := toml.DecodeFile("./config.toml", &cfg); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Method	URL")
+
+	router := httprouter.New()
+	port := cfg.Server.Port
+	for _, api := range cfg.APIs {
+		m := strings.ToLower(api.Method)
+		url := api.URL
+		fp := "./resp" + url
+
+		if _, err := os.Stat(fp); err != nil {
+			// ファイルが存在しないので無視
+			fmt.Println(fp + "が存在しません")
+			continue
+		}
+
+		fmt.Printf("%s	http://localhost:%s%s\n", m, port, url)
+
+		switch m {
+		case "get":
+			router.GET(url, func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+				b, err := ioutil.ReadFile(fp)
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Fprintf(w, string(b))
+			})
+		case "post":
+			router.POST(url, func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+				b, err := ioutil.ReadFile(fp)
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Fprintf(w, string(b))
+			})
+		default:
+			fmt.Println(m + "は不正なリクエストメソッドです。")
+		}
+	}
+
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
